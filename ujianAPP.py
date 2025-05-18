cdb30842d11265ac5cf7ce85a9c587bf0c4e2f2a
Panjol <beranicoba36@gmail.com>
2025-05-18 08:58:30 +0700

    Update ujianAPP.py

--- ujianAPP.py
+++ ujianAPP.py
@@ -15,8 +15,9 @@ def get_connection():
     return mysql.connector.connect(
         host="DB_HOST",
         user="DB_USER",
-        password="DB_NAME",
-        database="DB_PORT"
+        password="DB_PASSWORD",
+        database="DB_NAME",
+        port="DB_PORT"
     )
 
 
